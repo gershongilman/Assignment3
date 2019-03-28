dbd2044b007314f5f5c95adbdbc961a4e7859543
gershongilman <48959715+gershongilman@users.noreply.github.com>
2019-03-27 20:51:56 -0400

    Update TwinPrimeStream.java

--- TwinPrimeStream.java
+++ TwinPrimeStream.java
@@ -1,4 +1,46 @@
+package Stream;
+import util.IntegerPair;
+/**
+ * @author Gershon Gilman
+ * A stream that return integer pair's that represent two twin primes (primes seperate by 2)
+ */
+public class TwinPrimeStream extends Stream {
+	
+	//stores the IntegerPair we are working with
+	private IntegerPair currentPair;
+	
+	//storing the stream of primes
+	private PrimeStream primeStream;
+	
+	//we take no input but we know the first twin primes are (3,5) so I started with (2,3) so that we iterate in the program to return the (3,5) first
+	public TwinPrimeStream() {
+		currentPair = new IntegerPair(2, 3);
+		primeStream = new PrimeStream();
+			
+	}
 
-public class TwinPrimeStream {
-
+	/**
+	 * Iterate through primes to find pairs of primes that are two away from each other and returns this
+	 * @return a string with an integer pair of primes
+	 */
+	public IntegerPair next(){
+		IntegerPair out; 													//so that we can store the next value in the getters and setters we use this local variable
+		
+		//we know the main idea is the difference should be two so we iterate the values using prime stream till its true
+		while(currentPair.getValue1() + 2 != currentPair.getValue2()) {
+			currentPair.setValue1(currentPair.getValue2());
+			currentPair.setValue2(primeStream.next());
+		}
+		
+		out = currentPair;
+		currentPair.setValue1(currentPair.getValue2());
+		currentPair.setValue2(primeStream.next());
+		return out;
+	}
+	public static void main(String args[]) {
+		TwinPrimeStream test = new TwinPrimeStream();
+		System.out.println(test.next());
+		System.out.println(test.next());
+	}
+	
 }
