91066676bd028688d2922a7f201e6225d0578c0f
gershongilman <48959715+gershongilman@users.noreply.github.com>
2019-03-27 20:43:23 -0400

    Update TwinPrimeStream.java

--- TwinPrimeStream.java
+++ TwinPrimeStream.java
@@ -1,46 +1,40 @@
 package Stream;
 import util.IntegerPair;
-
+/**
+ * @author Gershon Gilman
+ * A stream that return integer pair's that represent two twin primes (primes seperate by 2)
+ */
 public class TwinPrimeStream extends Stream {
+	
+	//stores the IntegerPair we are working with
 	private IntegerPair currentPair;
+	
+	//storing the stream of primes
 	private PrimeStream primeStream;
+	
+	//we take no input but we know the first twin primes are (3,5) so I started with (2,3) so that we iterate in the program to return the (3,5) first
 	public TwinPrimeStream() {
-		setCurrentPair(new IntegerPair(3, 5));
+		currentPair = new IntegerPair(2, 3);
 		primeStream = new PrimeStream();
 			
 	}
 
-	public IntegerPair getCurrentPair() {
-		return currentPair;
-	}
-
-	public void setCurrentPair(IntegerPair pair) {
-		this.currentPair = pair;
-	}
-	
 	/**
-	 * @return the next value in the stream
+	 * Iterate through primes to find pairs of primes that are two away from each other and returns this
+	 * @return a string with an integer pair of primes
+	 * I may have misunderstood and we were supposed to return an integer pair type?!
 	 */
-	public IntegerPair next(){
-		IntegerPair out;
-
+	public String next(){
+		String out; 													//so that we can store the next value in the getters and setters we use this local variable
+		
+		//we know the main idea is the difference should be two so we iterate the values using prime stream till its true
 		while(currentPair.getValue1() + 2 != currentPair.getValue2()) {
-				currentPair.setValue1(currentPair.getValue2());
-				currentPair.setValue2(primeStream.next());
-				System.out.println("in line");
-			}
-			out = getCurrentPair();
 			currentPair.setValue1(currentPair.getValue2());
 			currentPair.setValue2(primeStream.next());
-			return out;
-	}
-		
-
-	public static void main(String args[]) {
-		TwinPrimeStream test = new TwinPrimeStream();
-		System.out.println(test.next());
-		System.out.println(test.next());
-		System.out.println(test.next());
-		System.out.println(test.next());
+		}
+		out = currentPair.toString();
+		currentPair.setValue1(currentPair.getValue2());
+		currentPair.setValue2(primeStream.next());
+		return out;
 	}
 }
